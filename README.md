a460a772ab64eed220b62d4c70041c3c21e21ad8
usefusefi <usefusefi@gmail.com>
2022-06-30 06:30:24 +0000

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Overview 
+This shows how we can create the whole GCP infra (in our case: storage bucket, and bigquery dataset) using Terraform in few simple steps (~5 minutes)
+
+## Local Setup for Terraform and GCP
+
+### Pre-Requisites
+1. Terraform client installation: https://www.terraform.io/downloads
+2. Cloud Provider account: https://console.cloud.google.com/ 
+
+### Terraform Setup
+[Terraform Overview](terraform_overview.md)
+
+### Terraform Workshop for GCP Infra
+Your setup is ready!
+Now head to the [terraform](terraform) directory, and perform the execution steps to create your infrastructure.
---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+locals {
+  data_lake_bucket = "gcp_with_tf" #replace it with your project id
+}
+
+variable "project" {
+  description = "Your GCP Project ID"
+}
+
+variable "region" {
+  description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
+  default = "europe-west6" #replace it with your region code 
+  type = string
+}
+
+variable "storage_class" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default = "STANDARD"
+}
+
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type = string
+  default = "bq_tf_dataset"
+}
